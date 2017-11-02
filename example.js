ede440d1b0d8aeeaae950785540a7b4dd2051e1b
FranciscoG <frankjg@gmail.com>
2017-11-02 19:20:10 -0400

    added media attribute support

--- example.js
+++ example.js
@@ -3,8 +3,10 @@ const stylesInline = require('./index.js');
 
 // load a local html as string
 var html = fs.readFileSync( './test/index.html', {"encoding" : "utf-8"} );
+
 // process it
 var converted = stylesInline(html, './test');
+
 // do whatever you want with it, for this example I'm saving it
 fs.ensureDirSync('./dist');
 fs.writeFileSync('./dist/index.html', converted);
--- index.js
+++ index.js
@@ -2,23 +2,47 @@ const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 
-function cssToStyle(cssHref, $) {
-  // read file as string  
+/**
+ * Loads a local CSS file and creates a <style> tag with its content
+ * 
+ * @param {string} cssHref the path to the local CSS file
+ * @param {Object} linkEl the <link> element repres
+ * @param {Object} $ updated Cheerio instance with new <style> tag
+ */
+function cssToStyle(cssHref, linkEl, $) {
   var css = fs.readFileSync(cssHref, { encoding: "utf-8"});
-  $('head').append(`<style data-href="${cssHref}" type="text/css">\n${css}\n</style>`);
+  
+  var styleAttr = [
+    `data-href="${cssHref}"`,
+    'type="text/css"'
+  ]
+  if (linkEl.attribs.media) {
+    styleAttr.push(`media="${linkEl.attribs.media}"`)
+  }
+
+  $('head').append(`<style ${styleAttr.join(' ')}>\n${css}\n</style>`);
   return $;
 }
 
-module.exports = function(htmlString, dir) {
+/**
+ * Takes in a string of valid HTML, uses cheerio to parse it for all link tags
+ * loads each link href and inlines the content inside a <style> tag in the <head>
+ * 
+ * @param {string} htmlString the valid html file as string
+ * @param {string} dir optional - the cwd
+ */
+function stylesInline(htmlString, dir) {
   var $ = cheerio.load(htmlString);
   dir = dir || "./";
 
   $('link').each(function(i,el){
     if (el.attribs.rel === "stylesheet") {
-      $ = cssToStyle( path.resolve(dir, el.attribs.href), $);
+      $ = cssToStyle( path.resolve(dir, el.attribs.href), el, $);
       $('head').remove(el);
     }
   });
 
   return $.html();
 }
+
+module.exports = stylesInline;
